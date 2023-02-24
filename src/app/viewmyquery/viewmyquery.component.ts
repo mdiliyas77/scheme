c1e9481c45a97a1aac9339eab952e3f57f59a73f
mdiliyas77 <mdiliyas.mugli@yahoo.com>
2023-02-24 19:22:03 +0530

    Changes

--- src/app/viewmyquery/viewmyquery.component.ts
+++ src/app/viewmyquery/viewmyquery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SchemeService } from '../scheme.service';
 
 @Component({
   selector: 'app-viewmyquery',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewmyqueryComponent implements OnInit {
 
-  constructor() { }
+  querylist: any = [];
+  myquerylist: any = [];
+  sortedarray: any = [];
+  model: any = {};
+  constructor(private schemeservice:SchemeService) { }
 
   ngOnInit(): void {
+    this.getQuery();
+  } 
+
+  getQuery() {
+    debugger;
+    this.schemeservice.GetAllQuery()
+      .subscribe({
+        next: (data) => {
+          this.querylist = data as any[];
+
+          this.myquerylist = this.querylist.find((myname)=>myname.memberid===sessionStorage.getItem('userid'));
+          this.model.name = this.myquerylist["name"];
+          
+        }
+      })
   }
+
+  ClearAll()
+  {
+    this.model.queryid="";
+    this.model.reply="";
+    this.model.memberid="";
+    this.model.name="";
+    this.model.schemeid="";
+    this.model.schemetitle="";
+    this.model.query="";
+  }
+
 
 }
